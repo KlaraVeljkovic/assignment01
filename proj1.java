dce6a7b10b44f0a37f75bd90816a3e17ece65be1
Klara Veljkovic <89892102+KlaraVeljkovic@users.noreply.github.com>
2024-09-03 11:59:20 -0400

    Create proj1.java

---
+++ proj1.java
@@ -0,0 +1,31 @@
+package assignment01;
+import java.util.ArrayList;
+public interface TrackCheck {
+  boolean check(ArrayList<String> list);
+}
+package assignment01;
+import java.util.ArrayList;
+public class Student {
+  private TrackCheck trackCheck;
+  private String bnumber;
+  private String name;
+  private ArrayList<String> courses;
+  public Student(String bnum, String nameIn) {
+    bnumber = bnum;
+    name = nameIn;
+    courses = new ArrayList<>();
+  }
+  public void setTrack(TrackCheck trackIn) {
+    trackCheck = trackIn;
+  }
+  public void add(String course) {
+    courses.add(course);
+  }
+  public boolean checkTrack() {
+    return trackCheck.check(courses);
+  }
+  @Override
+  public String toString() {
+  return name + " (" + bnumber + "): " + courses;
+  } 
+}
