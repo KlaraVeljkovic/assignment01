91bb96ffbc6da89d9fefa3f0f93e144e8d1738e8
Klara <kveljko1@binghamton.edu>
2024-09-03 12:11:43 -0400

    added from the pdf

--- proj1.java
+++ proj1.java
@@ -1,31 +1,50 @@
 package assignment01;
+import java.util.Set;  
+import java.util.TreeSet;
 import java.util.ArrayList;
-public interface TrackCheck {
+public interface TrackCheck 
+{
   boolean check(ArrayList<String> list);
 }
+
 package assignment01;
 import java.util.ArrayList;
-public class Student {
+public class Student 
+{
   private TrackCheck trackCheck;
   private String bnumber;
   private String name;
   private ArrayList<String> courses;
-  public Student(String bnum, String nameIn) {
+  public Student(String bnum, String nameIn) 
+  {
     bnumber = bnum;
     name = nameIn;
     courses = new ArrayList<>();
   }
-  public void setTrack(TrackCheck trackIn) {
+  public void setTrack(TrackCheck trackIn) 
+  {
     trackCheck = trackIn;
   }
-  public void add(String course) {
+  public void add(String course) 
+  {
     courses.add(course);
   }
-  public boolean checkTrack() {
+  public boolean checkTrack() 
+  {
     return trackCheck.check(courses);
   }
   @Override
-  public String toString() {
+  public String toString() 
+  {
   return name + " (" + bnumber + "): " + courses;
   } 
 }
+
+package assignment01;
+public class NoTrack implements TrackCheck ...
+
+package assignment01;
+public class AITrack implements TrackCheck ...
+
+package assignment01;
+public class CyberTrack implements TrackCheck ...
