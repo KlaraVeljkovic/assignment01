08b68cb1524f4659b6c422b6b9c1120caa619bab
Klara <kveljko1@binghamton.edu>
2024-09-09 16:00:25 -0400

    done

--- AITrack.java
+++ AITrack.java
@@ -5,12 +5,12 @@ import java.util.TreeSet;
 
 public class AITrack implements TrackCheck 
 {
-    private static final Set<String> aiElectiveSet = Set.of("CS515", "CS517", "CS517X", "CS524", "CS555", "CS556", "CS580E", "CS580P", "CS580N");
+    private static final Set<String> aiElectives = Set.of("CS515", "CS517", "CS517X", "CS524", "CS555", "CS556", "CS580E", "CS580P", "CS580N");
     @Override
     public boolean check(ArrayList<String> list) 
     {
-        Set<String> takenSet = new TreeSet<>(list);
-        takenSet.retainAll(aiElectiveSet);  
-        return takenSet.size() >= 2;
+        Set<String> finished = new TreeSet<>(list);
+        finished.retainAll(aiElectives);  
+        return finished.size() >= 2;
     }
 }
